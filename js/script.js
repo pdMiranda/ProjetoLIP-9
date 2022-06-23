81e04d55a193361eb784796f9958914d550c6bc1
Pedro Miranda <58314118+pdMiranda@users.noreply.github.com>
2022-06-23 20:53:33 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,145 @@
+(function ($) {
+  'use strict';
+  $(window).on('load', function () {
+    $('#preloader').fadeOut('slow', function () {
+      $(this).remove();
+    });
+  });
+  $('.hero-slider').slick({
+    autoplay: true,
+    infinite: true,
+    arrows: true,
+    prevArrow: '<button type=\'button\' class=\'prevArrow\'></button>',
+    nextArrow: '<button type=\'button\' class=\'nextArrow\'></button>',
+    dots: false,
+    autoplaySpeed: 7000,
+    pauseOnFocus: false,
+    pauseOnHover: false
+  });
+  $('.hero-slider').slickAnimation();
+
+  $(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+    //console.log(scroll);
+    if (scroll > 200) {
+      //console.log('a');
+      $('.navigation').addClass('sticky-header');
+    } else {
+      //console.log('a');
+      $('.navigation').removeClass('sticky-header');
+    }
+  });
+
+    setTimeout(function(){
+      var containerEl = document.querySelector('.filtr-container');
+      var filterizd;
+      if (containerEl) {
+        filterizd = $('.filtr-container').filterizr({});
+      }
+    }, 500);
+
+  $('.testimonial-slider').slick({
+    infinite: true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 2000
+  });
+
+  $('.clients-logo-slider').slick({
+    infinite: true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    slidesToShow: 5,
+    slidesToScroll: 1
+  });
+
+  $('.company-gallery').slick({
+    infinite: true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    slidesToShow: 5,
+    slidesToScroll: 1
+  });
+
+  $('#contact-form').validate({
+      rules: {
+        name: {
+          required: true,
+          minlength: 4
+        },
+        email: {
+          required: true,
+          email: true
+        },
+        subject: {
+          required: false
+        },
+        message: {
+          required: true
+        }
+      },
+      messages: {
+        user_name: {
+          required: 'Come on, you have a name don\'t you?',
+          minlength: 'Your name must consist of at least 2 characters'
+        },
+        email: {
+          required: 'Please put your email address'
+        },
+        message: {
+          required: 'Put some messages here?',
+          minlength: 'Your name must consist of at least 2 characters'
+        }
+      },
+      submitHandler: function (form) {
+        $(form).ajaxSubmit({
+          type: 'POST',
+          data: $(form).serialize(),
+          url: 'sendmail.php',
+          success: function () {
+            $('#contact-form #success').fadeIn();
+          },
+          error: function () {
+            $('#contact-form #error').fadeIn();
+          }
+        });
+      }
+    }
+
+  );
+
+  var scroll = new SmoothScroll('a[href*="#"]');
+
+  function counter() {
+    if ($('.counter').length !== 0) {
+      var oTop = $('.counter').offset().top - window.innerHeight;
+    }
+    if ($(window).scrollTop() > oTop) {
+      $('.counter').each(function () {
+        var $this = $(this),
+          countTo = $this.attr('data-count');
+        $({
+          countNum: $this.text()
+        }).animate({
+          countNum: countTo
+        }, {
+          duration: 1000,
+          easing: 'swing',
+          step: function () {
+            $this.text(Math.floor(this.countNum));
+          },
+          complete: function () {
+            $this.text(this.countNum);
+          }
+        });
+      });
+    }
+  }
+
+  $(window).scroll(function () {
+    counter();
+  });
+
+})(jQuery);
